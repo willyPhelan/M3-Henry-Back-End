f9c0372dada060fe0782e6ffd1e854742a657347
Franverri <francoetche@gmail.com>
2021-04-05 14:12:16 -0300

    Reorder 05-Express demos

--- 05-Express/demo/expressRoutes/app.js
+++ 05-Express/demo/expressRoutes/app.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express()
 
-
-
 app.get("/", function(req, res){
   res.send("Hello!")
 })
@@ -10,36 +8,39 @@ app.get("/", function(req, res){
 app.get("/welcome/:name", function(req, res){
   res.send(`Welcome ${capitalize(req.params.name)}`)
 })
+
 app.get("/hablar/:animal",function(req, res){
-  var animals={
+  var animals = {
     perro:"wuf",
     gato: "meow",
     vaca: "muuu"
   }
-  var animal=req.params.animal
+  var animal = req.params.animal
   var ruido = animals[animal]
-  if(ruido){
-    res.send(`El ${animal} hizo ${ruido}`)
+  if(ruido) {
+    return res.send(`El ${animal} hizo ${ruido}`)
   }
   res.redirect("/error")
-  } )
+})
 
 app.get("/repeat/:word/:times", function(req, res){
-  var string=""
+  var string = ""
   if(parseInt(req.params.times)){
-  for(var i=0; i<req.params.times; i++){
-    string+="<p>"+req.params.word+"</p>"
-  }}else{
-    string="Tenes q poner un numero de veces como ultimo parametro"
+  for(var i=0; i < req.params.times; i++) {
+    string += "<p>" + req.params.word + "</p>"
+  }} else {
+    string = "Tenes q poner un numero de veces como ultimo parametro"
   }
   res.send(string)
 })
-  app.get("/error", function(req, res){
-    res.send("Cannot be Found!")
-  })
-  app.get("*", function(req, res){
-    res.redirect("/error")
-  })
+
+app.get("/error", function(req, res){
+  res.send("Cannot be Found!")
+})
+
+app.get("*", function(req, res){
+  res.redirect("/error")
+})
 
 app.listen(8000)
 
