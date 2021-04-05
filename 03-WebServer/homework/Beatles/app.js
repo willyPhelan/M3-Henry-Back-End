a6f4d68373d161bb727b314d81ab3a45a4b880ae
Franverri <francoetche@gmail.com>
2021-04-05 14:01:58 -0300

    Change image URLs from beatles that were not working

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -4,7 +4,7 @@ var fs   = require('fs');
 var beatles=[{
   name: "John Lennon",
   birthdate: "09/10/1940",
-  profilePic:"http://beatlephotoblog.com/photos/2013/05/132.jpg32.jpg"
+  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
 },
 {
   name: "Paul McCartney",
@@ -14,7 +14,7 @@ var beatles=[{
 {
   name: "George Harrison",
   birthdate: "25/02/1946",
-  profilePic:"http://az616578.vo.msecnd.net/files/2016/03/09/635931448636931925-692833716_george-harrison-living-in-the-material-world-george-harrison-photo-credit-credit-robert-whitaker-c-apple-corps-ltd-courtesy-of-hbo.jpg"
+  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
 },
 {
   name: "Richard Starkey",
@@ -22,4 +22,3 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
-
