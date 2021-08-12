8a88068e28ed47298312adfec254b67dc879bd19
Franverri <francoetche@gmail.com>
2021-08-12 10:22:40 -0300

    Remove body parser

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json()); // for parsing application/json
+app.use(express.json()); // for parsing application/json
 
 app.get('/', (req, res) => {
   res.send({
